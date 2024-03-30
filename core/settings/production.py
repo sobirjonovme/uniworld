ba5c371423a30088303b17355e700b185670ce3c
Asadbek <huntsman6004@gmail.com>
2024-03-30 13:50:45 +0500

    update: production config

--- core/settings/production.py
+++ core/settings/production.py
@@ -10,18 +10,16 @@ DEBUG = True
 # Django security
 ###################################################################
 
-# USE_X_FORWARDED_HOST = True
-# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 CSRF_COOKIE_SECURE = True
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost",
     "http://127.0.0.1",
     "http://127.0.0.1:8000",
-    "http://heatwarehouse.pythonanywhere.com",
-    "https://heatwarehouse.pythonanywhere.com",
-    "http://heat-warehouse.xn--0h8h.uz",
-    "https://heat-warehouse.xn--0h8h.uz",
+    "http://uniworld.xn--0h8h.uz",
+    "https://uniworld.xn--0h8h.uz",
 ]
 
 ###################################################################
